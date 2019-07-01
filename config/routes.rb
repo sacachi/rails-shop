6a87bc385e32d9e65a9da72999da70846e576145
sacachi <sacachi@gmail.com>
2019-07-01 09:25:17 +0700

    change URL category and product to shop_account folder

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :categories
-  resources :products
+  namespace :shop_account do
+    resources :categories
+    resources :products
+  end
+  resources :categories, only: [:show, :index]
+  resources :products, only: [:show, :index]
   get 'shop_admin/index'
   root to: 'pages#home'
   devise_for :shop_accounts
